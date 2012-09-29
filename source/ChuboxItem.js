e1356646e31b3a8edb390093fb59042aefd35e85
Martin Kleinschrodt <martin@maklesoft.com>
2012-09-29 12:18:30 +0200

    Rename ChuboxListItem to ListChuboxItem and put into ChuboxItem.js

--- source/ChuboxItem.js
+++ source/ChuboxItem.js
@@ -19,3 +19,28 @@ enyo.kind({
 		{classes: "chuboxitem-caption ellipsis", name: "caption"}
 	]
 });
+
+enyo.kind({
+	name: "ListChuboxItem",
+	kind: "onyx.Item",
+	layoutKind: "FittableColumnsLayout",
+	classes: "listchuboxitem",
+	noStretch: true,
+	published: {
+		item: null
+	},
+	itemChanged: function() {
+		if (this.item) {
+			this.$.caption.setContent(this.item.product.name);
+			this.$.productImage.applyStyle("background-image", "url(" + this.item.product.image_1 + ")");
+		}
+	},
+	create: function() {
+		this.inherited(arguments);
+		this.itemChanged();
+	},
+	components: [
+		{classes: "listchuboxitem-image", name: "productImage"},
+		{classes: "listchuboxitem-caption ellipsis", name: "caption", fit: true}
+	]
+});
