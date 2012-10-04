3600e66173658343da111332cc5135dd6233479a
Martin Kleinschrodt <martin@maklesoft.com>
2012-10-04 11:28:02 +0200

    Add a list of users chu to profile view

--- source/ProfileView.js
+++ source/ProfileView.js
@@ -12,14 +12,40 @@ enyo.kind({
 			this.$.fullName.setContent(this.showedUser.profile.first_name + " " + this.showedUser.profile.last_name);
 			this.$.userName.setContent(this.showedUser.username);
 			this.$.bio.setContent(this.showedUser.profile.bio);
+			this.loadChus();
 		}
 	},
+    loadChus: function() {
+        chuisy.chu.list([["user", this.showedUser.id]], enyo.bind(this, function(sender, response) {
+            this.chus = response.objects;
+            this.refreshChus();
+        }));
+    },
+    refreshChus: function() {
+        this.$.chuList.setCount(this.chus.length);
+        this.$.chuList.render();
+    },
+    setupChu: function(sender, event) {
+        var chu = this.chus[event.index];
+        event.item.$.listChu.setChu(chu);
+    },
+    chuTapped: function(sender, event) {
+        var chu = this.chus[event.index];
+        this.doChuSelected({chu: chu});
+    },
 	components: [
-		{kind: "Scroller", fit: true, components: [
-			{kind: "Image", classes: "profileview-avatar", name: "avatar"},
-			{classes: "profileview-fullname", name: "fullName"},
-			{classes: "profileview-username", name: "userName"},
-			{classes: "profileview-bio", name: "bio"}
+		{kind: "Scroller", classes: "profileview-mainpanel", fit: true, components: [
+			{classes: "pageheader", components: [
+				{kind: "Image", classes: "profileview-avatar", name: "avatar"},
+				{classes: "profileview-profileinfo", components: [
+					{classes: "profileview-fullname", name: "fullName"},
+					{classes: "profileview-username", name: "userName"},
+					{classes: "profileview-bio", name: "bio"}
+				]}
+			]},
+            {kind: "Repeater", name: "chuList", onSetupItem: "setupChu", components: [
+                {kind: "ListChu", ontap: "chuTapped", style: "width: 100%;"}
+            ]}
 		]},
 		{classes: "secondarypanels shadow-left"}
 	]
