5a265bc3a2a692aa9a02929bc2e2aa9bc579f802
Martin Kleinschrodt <martin@maklesoft.com>
2012-11-15 11:18:12 +0100

    A couple of styling changes on the ChuFeedItem

--- source/ChuFeedItem.js
+++ source/ChuFeedItem.js
@@ -6,14 +6,14 @@ enyo.kind({
 	},
 	chuChanged: function() {
 		if (this.chu) {
-			this.$.image.setSrc(this.chu.thumbnails["300x300"] || "assets/images/chu_placeholder.png");
+			this.$.image.applyStyle("background-image", "url(" + (this.chu.thumbnails["300x300"] || "assets/images/chu_placeholder.png") + ")");
 			this.$.avatar.setSrc(this.chu.user.profile.avatar_thumbnail || "assets/images/avatar_placeholder.png");
 			this.$.username.setContent(this.chu.user.username);
 			this.$.time.setContent(chuisy.timeToText(this.chu.time));
 		}
-	},  
+	},
 	components: [
-		{kind: "Image", name: "image", classes: "chufeeditem-image"},
+		{name: "image", classes: "chufeeditem-image"},
 		{kind: "Image", classes: "chufeeditem-avatar miniavatar", name: "avatar"},
 		{classes: "chufeeditem-username ellipsis", name: "username"},
 		{classes: "chufeeditem-time", name: "time"}
