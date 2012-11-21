64c6a1b091f13cf7c42ae9b5ce729d00c662e741
Martin Kleinschrodt <martin@maklesoft.com>
2012-11-21 08:55:27 +0100

    Show likes and comments count in chu feed

--- source/ChuFeedItem.js
+++ source/ChuFeedItem.js
@@ -10,11 +10,23 @@ enyo.kind({
 			this.$.avatar.setSrc(this.chu.user.profile.avatar_thumbnail || this.chu.user.profile.avatar || "assets/images/avatar_placeholder.png");
 			this.$.username.setContent(this.chu.user.username);
 			this.$.time.setContent(chuisy.timeToText(this.chu.time));
+			this.$.likesCount.setContent(this.chu.likes_count);
+			this.$.commentsCount.setContent(this.chu.comments_count);
 		}
 	},
 	components: [
 		{name: "image", classes: "chufeeditem-image"},
-		{kind: "Image", classes: "chufeeditem-avatar miniavatar", name: "avatar"},
+		{classes: "chufeeditem-likes-comments", components: [
+			{classes: "chufeeditem-likes", components: [
+				{classes: "chufeeditem-likes-count", name: "likesCount"},
+				{classes: "chufeeditem-likes-icon"}
+			]},
+			{classes: "chufeeditem-comments", components: [
+				{classes: "chufeeditem-comments-count", name: "commentsCount"},
+				{classes: "chufeeditem-comments-icon"}
+			]}
+		]},
+		{kind: "Image", classes: "chufeeditem-avatar", name: "avatar"},
 		{classes: "chufeeditem-username ellipsis", name: "username"},
 		{classes: "chufeeditem-time", name: "time"}
 	]
