ce495312affd1852e53a8794083c02fc6a577a98
Martin Kleinschrodt <martin@maklesoft.com>
2012-12-10 11:17:45 +0100

    Decrease foto quality to avoid memory error;

--- source/ComposeChu.js
+++ source/ComposeChu.js
@@ -15,7 +15,7 @@ enyo.kind({
             navigator.camera.getPicture(enyo.bind(this, this.gotImage), enyo.bind(this, function() {
                 this.warn("Getting image failed!");
                 this.doBack();
-            }), {targetWidth: 768, targetHeight: 1024, correctOrientation: true});
+            }), {targetWidth: 768, targetHeight: 1024, correctOrientation: true, quality: 49});
         } catch (e) {
             this.warn("No camera available!");
             this.gotImage("");
