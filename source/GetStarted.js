b83c1439d5eb441c5a282fd855273a850a1c719f
Martin Kleinschrodt <martin@maklesoft.com>
2013-02-17 14:35:25 +0100

    Add invite-friends-view to get started view

--- source/GetStarted.js
+++ source/GetStarted.js
@@ -13,9 +13,13 @@ enyo.kind({
         if (App.checkConnection()) {
             App.loginWithFacebook(enyo.bind(this, function(accessToken) {
                 this.$.spinner.show();
-                chuisy.signIn(accessToken, enyo.bind(this, function() {
+                chuisy.signIn(accessToken, enyo.bind(this, function(user) {
                     this.$.spinner.hide();
-                    this.doDone();
+                    user.fbFriends.fetchAll();
+                    this.$.panels.setIndex(1);
+                    enyo.asyncMethod(this, function() {
+                        this.$.inviteFriendsPanel.reflow();
+                    });
                 }), enyo.bind(this, function() {
                     this.$.spinner.hide();
                     navigator.notification.alert($L("Hm, that didn't work. Please try it again later!"), enyo.bind(this, function() {
@@ -27,9 +31,26 @@ enyo.kind({
             this.doDone();
         }
     },
+    inviteFriendsContinue: function() {
+        var selectedFriends = this.$.fbFriendsPicker.getIds();
+        if (selectedFriends.length) {
+            FB.ui({
+                method: "apprequests",
+                message: $L("Come join me on Chuisy, share beautiful fashion with me and help me decide on shopping decisions!"),
+                to: selectedFriends
+            }, enyo.bind(this, function(response) {
+                if (response.request) {
+                    // User did send the request
+                    this.doDone();
+                }
+            }));
+        } else {
+            this.doDone();
+        }
+    },
     components: [
-        {kind: "Panels", classes: "enyo-fill", components: [
-            {classes: "getstarted-login", components: [
+        {kind: "Panels", arrangerKind: "CarouselArranger", classes: "enyo-fill", components: [
+            {classes: "getstarted-login enyo-fill", components: [
                 {classes: "getstarted-login-logo"},
                 {classes: "getstarted-login-buttons", components: [
                     {kind: "onyx.Button", name: "facebookButton", classes: "facebook-button", ontap: "signIn", components: [
@@ -38,6 +59,13 @@ enyo.kind({
                     ]},
                     {kind: "onyx.Button", content: $L("Skip"), ontap: "doDone"},
                     {kind: "onyx.Spinner", classes: "getstarted-login-spinner", showing: false}
+                ]}
+            ]},
+            {kind: "FittableRows", name: "inviteFriendsPanel", classes: "enyo-fill", components: [
+                {classes: "getstarted-invitefriends-text", content: $L("Chuisy is much more fun to use with other people. Invite your friends to Chuisy now!")},
+                {kind: "FbFriendsPicker", fit: true},
+                {classes: "getstarted-invitefriends-continue", components: [
+                    {kind: "onyx.Button", classes: "getstarted-invitefriends-continue-button", content: $L("Continue"), ontap: "inviteFriendsContinue"}
                 ]}
             ]}
         ]}
