62208ac76f3c635fbbb9712d2172bfc641041794
Martin Kleinschrodt <martin@maklesoft.com>
2013-06-15 19:23:38 +0200

    Add support for asynchronous in/out animations; Add published properties for default in/out animations; Add new animation

--- source/AnimatedPanels.js
+++ source/AnimatedPanels.js
@@ -10,17 +10,35 @@ enyo.kind({
         SLIDE_OUT_TO_RIGHT: "slideOutToRight",
         SLIDE_IN_FROM_BOTTOM: "slideInFromBottom",
         SLIDE_OUT_TO_TOP: "slideOutToTop",
+        FADE_OUT: "fadeOut",
+        FADE_IN: "fadeIn",
         NONE: "none"
     },
     events: {
         onAnimationEnd: ""
+    },
+    published: {
+        // If true, the out animation of the old panel will be started as soon as possible
+        // instead of waiting for the new panel to be rendered and painted. The result
+        // is a decoupled in and out animation
+        async: false,
+        inAnim: "slideInFromRight",
+        outAnim: "slideOutToLeft"
     },
     create: function() {
         this.inherited(arguments);
         this.currentPanel = this.getClientControls()[0];
         // this.currentPanel.applyStyle("-webkit-transform", "translate3d(0, 0, 0)");
         this.currentPanel.applyStyle("display", "block");
+        this.animationStartHandler = enyo.bind(this, this.animationStart);
         this.animationEndHandler = enyo.bind(this, this.animationEnd);
+    },
+    animationStart: function() {
+        this.currentPanel.hasNode().removeEventListener("webkitAnimationStart", this.animationStartHandler, false);
+        this.newPanel.applyStyle("display", "block");
+        this.newPanel.applyStyle("-webkit-animation", this.currInAnim + " 0.5s");
+        this.newPanel.applyStyle("opacity", 1);
+        this.newPanel.resized();
     },
     animationEnd: function() {
         // this.newPanel.applyStyle("-webkit-transform", "translate3d(0, 0, 0)");
@@ -42,19 +60,22 @@ enyo.kind({
             // this.log("This panel is already selected.");
             return;
         }
-        inAnim = inAnim || AnimatedPanels.SLIDE_IN_FROM_RIGHT;
-        outAnim = outAnim || AnimatedPanels.SLIDE_OUT_TO_LEFT;
+        this.currInAnim = inAnim || this.inAnim;
+        outAnim = outAnim || this.outAnim;
         if (this.animating) {
             this.animationEnd();
         }
         this.newPanel = panel;
         this.newPanel.applyStyle("opacity", 0);
-        this.newPanel.applyStyle("display", "block");
         this.animating = true;
         this.currentPanel.hasNode().addEventListener("webkitAnimationEnd", this.animationEndHandler, false);
+
+        if (this.async) {
+            this.currentPanel.hasNode().addEventListener("webkitAnimationStart", this.animationStartHandler, false);
+        } else {
+            this.animationStart();
+        }
         this.currentPanel.applyStyle("-webkit-animation", outAnim + " 0.5s");
-        this.newPanel.applyStyle("-webkit-animation", inAnim + " 0.5s");
-        this.newPanel.applyStyle("opacity", 1);
     },
     selectDirect: function(panel) {
         if (this.currentPanel == panel) {
