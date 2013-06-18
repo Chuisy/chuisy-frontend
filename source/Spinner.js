e49596dcbc20977f20bb7585c6d6a974cf6d20df
Martin Kleinschrodt <martin@maklesoft.com>
2013-06-18 16:34:46 +0200

    Add ability to start/stop spinner

--- source/Spinner.js
+++ source/Spinner.js
@@ -2,14 +2,19 @@ enyo.kind({
     name: "Spinner",
     classes: "spinner",
     published: {
-        light: false
+        light: false,
+        spinning: true
     },
     create: function() {
         this.inherited(arguments);
         this.lightChanged();
+        this.spinningChanged();
     },
     lightChanged: function() {
         this.addRemoveClass("light", this.light);
+    },
+    spinningChanged: function() {
+        this.addRemoveClass("spin", this.spinning);
     },
     components: [
         {classes: "spinner-wheel"}
