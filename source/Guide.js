438549f81b0ab3f283bb11abdc81ec5eefc563c3
Martin Kleinschrodt <martin@maklesoft.com>
2013-07-05 17:12:20 +0200

    Refine guide design

--- source/Guide.js
+++ source/Guide.js
@@ -42,35 +42,26 @@ enyo.kind({
         ]},
         {kind: "Carousel", name: "panels", onTransitionStart: "panelsTransitionStart", fit: true, components: [
             {kind: "Card", classes: "enyo-fill", components: [
-                {classes: "guide-card-side", components: [
+                {classes: "guide-card-side", style: "background: url(assets/images/guide_front_1.jpg) no-repeat center bottom #fff; background-size: 300px 153px;", components: [
                     {classes: "guide-card-title", content: $L("Discover fashion")},
-                    {classes: "guide-card-text", content: $L("Discover the newest and prettiest fashion items in the stores of your city. Find out where others go shopping and gain inspiration for your own shopping trips."), allowHtml: true, style: "padding: 0px 30px 15px 30px;"},
-                    {kind: "Image", src: "assets/images/guide_front_1.png", style: "width: 220px;"}
+                    {classes: "guide-card-text", content: $L("Discover the prettiest fashion items in your city. Find out where others go shopping and find inspiration for your own shopping trips.")}
                 ]},
                 {classes: "guide-card-side"}
             ]},
             {kind: "Card", classes: "enyo-fill", components: [
                 {classes: "guide-card-side", components: [
                     {classes: "guide-card-title", content: $L("Snap fashion items")},
-                    {classes: "guide-card-text", content: $L("Take pictures of fashion items you adore. You decide whether you want to share your discoveries with your friends or keep them to yourself.")},
-                    {kind: "Image", src: "assets/images/guide_front_2.png", style: "width: 100px; margin: 12px;"}
-                ]},
-                {classes: "guide-card-side"}
-            ]},
-            {kind: "Card", classes: "enyo-fill", components: [
-                {classes: "guide-card-side", components: [
-                    {classes: "guide-card-title", content: $L("Collect gifts")},
-                    {classes: "guide-card-text", content: $L("You can receive unique gifts by posting products on Chuisy. Collect discounts on your favourite pieces and get many other small goodies!")},
-                    {kind: "Image", src: "assets/images/guide_front_3.png", style: "width: 250px;"}
+                    {classes: "guide-card-text", content: $L("Take pictures of your favourite fashion items while shopping and collect gifts. You decide if you want to share your discoveries with your friends or keep them to yourself."), style: "padding: 10px 25px 25px 25px;"},
+                    {kind: "Image", src: "assets/images/guide_front_2.png", style: "width: 200px; height: 100px"}
                 ]},
                 {classes: "guide-card-side"}
             ]},
             {kind: "Card", classes: "enyo-fill", components: [
                 {classes: "guide-card-side", components: [
                     {classes: "guide-card-title", content: $L("Join now")},
-                    {classes: "guide-card-text", style: "padding: 10px 25px;", content: $L("You can only connect with your friends and collect unlimited goodies if you sign in. Your data is safe with us and we won't post anything without asking you!")},
-                    {kind: "SignInButton", onSignInSuccess: "doDone", onSignInFail: "doDone", style: "margin-top: 20px;"},
-                    {kind: "Button", content: $L("Browse anonymously"), style: "width: 240px; margin-top: 8px;", ontap: "doDone"}
+                    {classes: "guide-card-text", content: $L("If you sign in you can connect with your friends and use all of Chuisy's features. Your data is safe with us and we won't post anything without asking you!"), style: "padding: 10px 25px 30px 25px;"},
+                    {kind: "SignInButton", onSignInSuccess: "doDone", onSignInFail: "doDone"},
+                    {kind: "Button", content: $L("Browse anonymously"), classes: "signin-cancel-button", ontap: "doDone"}
                 ]},
                 {classes: "guide-card-side"}
             ]}
@@ -78,8 +69,7 @@ enyo.kind({
         {kind: "Group", name: "ghosts", classes: "guide-card-ghost", style: "margin-bottom: 15px;", components: [
             {kind: "Button", classes: "guide-card-ghost-bullet", ontap: "stepSelected", value: 0, active: true},
             {kind: "Button", classes: "guide-card-ghost-bullet", ontap: "stepSelected", value: 1},
-            {kind: "Button", classes: "guide-card-ghost-bullet", ontap: "stepSelected", value: 2},
-            {kind: "Button", classes: "guide-card-ghost-bullet", ontap: "stepSelected", value: 3}
+            {kind: "Button", classes: "guide-card-ghost-bullet", ontap: "stepSelected", value: 2}
         ]}
     ]
 });
